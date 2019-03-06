bf4bb516a8794ef16003a1fee3288b5e2b479cd6
Blickwinkel <1196061706@qq.com>
2019-03-06 23:24:02 +0800

    Updated Body.java

--- src/generate/Body.java
+++ src/generate/Body.java
@@ -17,10 +17,12 @@ import org.apache.commons.io.filefilter.DirectoryFileFilter;
 import org.apache.commons.io.filefilter.FileFilterUtils;
 import org.eclipse.jgit.api.errors.*;
 import org.eclipse.jgit.internal.storage.file.FileRepository;
+import org.sqlite.SQLiteException;
+
 
 public class Body {
 	@SuppressWarnings("unused")
-	public static void main(String[] args) throws IOException, RefAlreadyExistsException, RefNotFoundException, InvalidRefNameException, CheckoutConflictException, GitAPIException, SQLException {
+	public static void main(String[] args) throws IOException, RefAlreadyExistsException, RefNotFoundException, InvalidRefNameException, CheckoutConflictException, GitAPIException, SQLException, SQLiteException {
 		//needless requirement path!
 		String reqPath = "C:\\Users\\24541\\Desktop\\req-swing-demo\\data\\sample\\AquaLush_Requirement";
 		
@@ -50,14 +52,11 @@ public class Body {
 			String name = rs_names.getString("name");
 			System.out.println(name);
 			
-			double t0 = System.currentTimeMillis();
 			
 			NewRetro re = new NewRetro();
 			
-			if (!name.equals("okhttp"))
-				continue;
-			
-			String localRepoPath = "F:\\创新项目\\sample projects\\"+name;
+//			String localRepoPath = "F:\\创新项目\\sample projects\\"+name;
+			String localRepoPath = "E:\\Desktop\\workspace\\创新项目：紧密度追踪过时需求\\project1006\\"+name;
 			int repoNum = 0;
 			ResultSet repoNumSet = stmt.executeQuery("SELECT repoNum FROM repositories WHERE name = '" + name + "'");
 			if (repoNumSet.next()) {
@@ -66,36 +65,46 @@ public class Body {
 			System.out.println("repoNum:--------" + repoNum);
 			
 			List<String> shas = new ArrayList<String>();
- 			ResultSet shaSet_test = stmt.executeQuery("SELECT sha FROM test WHERE repoNum = " + repoNum);
+ 			ResultSet shaSet_test = stmt.executeQuery("SELECT sha, note FROM test WHERE repoNum = " + repoNum);
 			while(shaSet_test.next()) {
 				String sha = shaSet_test.getString("sha");
-				System.out.println(sha);
+				String note = shaSet_test.getString("note");
+				if(note != null) {		//if it has been parsed as "noJavaFiles" or <Exceptions>
+					System.out.println(sha + " done: " + note);
+					continue;
+				}
+//				System.out.println(sha);
 				shas.add(sha);
 			}
-			ResultSet shaSet_train = stmt.executeQuery("SELECT sha FROM train WHERE repoNum = " + repoNum);
+			ResultSet shaSet_train = stmt.executeQuery("SELECT sha, note FROM train WHERE repoNum = " + repoNum);
 			while(shaSet_train.next()) {
 				String sha = shaSet_train.getString("sha");
-				System.out.println(sha);
+				String note = shaSet_train.getString("note");
+				if(note != null) {		//if it has been parsed as "noJavaFiles" or <Exceptions>
+					System.out.println(sha + " done: " + note);
+					continue;
+				}
+//				System.out.println(sha);
 				shas.add(sha);
 			}
-			ResultSet shaSet_valid = stmt.executeQuery("SELECT sha FROM valid WHERE repoNum = " + repoNum);
+			ResultSet shaSet_valid = stmt.executeQuery("SELECT sha, note FROM valid WHERE repoNum = " + repoNum);
 			while(shaSet_valid.next()) {
 				String sha = shaSet_valid.getString("sha");
-				System.out.println(sha);
+				String note = shaSet_valid.getString("note");
+				if(note != null) {		//if it has been parsed as "noJavaFiles" or <Exceptions>
+					System.out.println(sha + " done: " + note);
+					continue;
+				}
+//				System.out.println(sha);
 				shas.add(sha);
 			}
 			
 			for(String each : shas) {
+				double t0 = System.currentTimeMillis();
 				//String newCommit = "757f8ede8936402e576a16988f50994eda41f7a1";
 				String newCommit = each;
 				String rollBack = newCommit + "~1";
-				
-				stmt.close();
-				c.commit();
-				c.close();
-				
-				Boolean isSaved = false;
-				
+				Boolean isSaved = false;	
 				File localVersion = new File(localRepoPath);
 				
 				boolean hasJavaFiles = GitUtils.hasJavaFiles(localVersion, newCommit);
@@ -104,7 +113,12 @@ public class Body {
 					System.out.println("Commit: " + newCommit);
 					double t1 = System.currentTimeMillis();
 					System.out.println("Finished.\nTime used: " + ((t1 - t0) / 1000) + "s");
-					return;
+					System.out.println("UPDATE _ SET note = 'noJavaFiles' WHERE sha = '"+each+"' AND repoNum = "+repoNum);
+					stmt.executeUpdate("UPDATE test SET note = 'noJavaFiles' WHERE sha = '"+each+"' AND repoNum = "+repoNum);
+					stmt.executeUpdate("UPDATE train SET note = 'noJavaFiles' WHERE sha = '"+each+"' AND repoNum = "+repoNum);
+					stmt.executeUpdate("UPDATE valid SET note = 'noJavaFiles' WHERE sha = '"+each+"' AND repoNum = "+repoNum);
+					c.commit();
+					continue;
 				}
 			
 				//get current ObjectId in order to checkout back
@@ -140,24 +154,27 @@ public class Body {
 				try {
 					re.process(newJavaPath.getAbsolutePath(), oldJavaPath.getAbsolutePath(), reqPath, isSaved);
 				}catch(Exception e) {
-					StringWriter sw = null;
-					PrintWriter pw = null;
-					System.out.println(e);
-					sw = new StringWriter();
-					pw = new PrintWriter(sw);
-					e.printStackTrace(pw);
-					pw.flush();
-					sw.flush();
+					System.out.println(e.getClass().getName());
+					System.out.println("UPDATE _ SET note = '"+e.getClass().getName()+"' WHERE sha = '"+each+"' AND repoNum = "+repoNum);
+					stmt.executeUpdate("UPDATE test SET note = '"+e.getClass().getName()+"' WHERE sha = '"+each+"' AND repoNum = "+repoNum);
+					stmt.executeUpdate("UPDATE train SET note = '"+e.getClass().getName()+"' WHERE sha = '"+each+"' AND repoNum = "+repoNum);
+					stmt.executeUpdate("UPDATE valid SET note = '"+e.getClass().getName()+"' WHERE sha = '"+each+"' AND repoNum = "+repoNum);
+					c.commit();
 					if (!isSaved) {
 						Boolean deleteNewJava = FileUtils.deleteQuietly(newJavaPath);
 						Boolean deleteOldJava = FileUtils.deleteQuietly(oldJavaPath);
 					}
-					return;
+					continue;
 				}
 				if (!isSaved) {
 					Boolean deleteNewJava = FileUtils.deleteQuietly(newJavaPath);
 					Boolean deleteOldJava = FileUtils.deleteQuietly(oldJavaPath);
 				}
+				System.out.println("UPDATE _ SET note = 'OK' WHERE sha = '"+each+"' AND repoNum = "+repoNum);
+				stmt.executeUpdate("UPDATE test SET note = 'OK' WHERE sha = '"+each+"' AND repoNum = "+repoNum);
+				stmt.executeUpdate("UPDATE train SET note = 'OK' WHERE sha = '"+each+"' AND repoNum = "+repoNum);
+				stmt.executeUpdate("UPDATE valid SET note = 'OK' WHERE sha = '"+each+"' AND repoNum = "+repoNum);
+				c.commit();
 				double t1 = System.currentTimeMillis();
 				System.out.println("Finished.\nTime used: " + ((t1 - t0) / 1000) + "s");
 			}
